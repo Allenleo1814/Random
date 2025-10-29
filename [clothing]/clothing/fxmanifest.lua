59726446612cc779b84429ecb558b1a90be56e37
Allenleo1814 <allenleonelperez@gmail.com>
2025-10-29 01:36:17 -0400

    add

---
+++ [clothing]/clothing/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'mp_f_freemode_01_gr8.meta',
+    'mp_m_freemode_01_gr8.meta',
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_gr8.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_gr8.meta'
+
